2107623406e7b4b1e54db9539707d29f9a8d81ff
chic-caller-7520 <arbrown110@gmail.com>
2020-04-08 18:27:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,18 +1,20 @@
 class Book
-  attr_accessor :author, :page_count
-  attr_reader :title, :genre
-
-  GENRE= []
-
+  attr_accessor :author, :page_count  
+  attr_reader :title, :genre  
+ 
+  GENRES = []
+ 
   def initialize(title)
     @title = title
   end
-
+ 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+ 
+ 
   def genre=(genre)
     @genre = genre
-    GENRE << genre
-  end  
+    GENRES << genre 
+  end
 end
